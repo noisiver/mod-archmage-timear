87017b81a60cba8bc11f77d201a1484ea110e221
Revision <tkn963@gmail.com>
2024-05-22 00:52:33 +0200

    Lan'dalock
    Added an override for Archmage Lan'dalock. It uses the same system as Timear and my progression module, which is more blizzlike and therefore better.

--- src/loader.cpp
+++ src/loader.cpp
@@ -1,6 +1,8 @@
 void AddSC_npc_archmage_timear();
+void AddSC_npc_archmage_landalock_default();
 
 void Addmod_archmage_timearScripts()
 {
     AddSC_npc_archmage_timear();
+    AddSC_npc_archmage_landalock_default();
 }
